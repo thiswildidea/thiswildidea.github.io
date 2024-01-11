1c34f14cbb13e115532398003b3949dcb82984ab
thiswildidea <gaqo@outlook.com>
2024-01-11 09:05:40 +0800

    第一个地图完整版

--- jsapi/4.28/esri/widgets/support/dataUtils.js
+++
@@ -1,4 +0,0 @@
-// All material copyright Esri, All Rights Reserved, unless otherwise specified.
-// See https://js.arcgis.com/4.28/esri/copyright.txt for details.
-//>>built
-define(["exports"],function(b){b.isNumber=function(a){return"number"===typeof a};b.isString=function(a){return"string"===typeof a||a instanceof String};Object.defineProperty(b,Symbol.toStringTag,{value:"Module"})});
