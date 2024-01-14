fc4f97ac07af8554345a5159ef7f70c12f767c27
thiswildidea <gaqo@outlook.com>
2024-01-14 19:57:55 +0800

    tt

---
+++ assets/index.1705233370182107.js
@@ -0,0 +1,1 @@
+import{at as o}from"./index.1705233370182.js";function e(){return o({url:"/config/demo/earthquakes/earthquakes.json",method:"get"})}function r(){return o({url:"/config/demo/cors/cors.json",method:"get"})}function t(){return o({url:"/config/demo/cors/corsdata.json",method:"get"})}export{r as a,e as b,t as g};
